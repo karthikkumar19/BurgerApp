de825c36cc4e837367a8f90ff68d6f2bf56416a8
Karthik kumar <mkarthikkumar19@gmail.com>
2019-12-26 22:10:27 +0530

    Updates

---
+++ precache-manifest.63d96694244c23f3ad8cc293a97fc688.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09cedc6bf6b7f5d21a00bbb6ca68053a",
+    "url": "/karthikkumar19/BurgerApp.git/index.html"
+  },
+  {
+    "revision": "24cc7a24496ee787fdf5",
+    "url": "/karthikkumar19/BurgerApp.git/static/css/3.69e35093.chunk.css"
+  },
+  {
+    "revision": "a96e1878552606d3ba8c",
+    "url": "/karthikkumar19/BurgerApp.git/static/css/4.157dfab5.chunk.css"
+  },
+  {
+    "revision": "69a0f84598621378ee0d",
+    "url": "/karthikkumar19/BurgerApp.git/static/css/5.0c143387.chunk.css"
+  },
+  {
+    "revision": "a7ef5960babbf106638e",
+    "url": "/karthikkumar19/BurgerApp.git/static/css/main.f7f587a2.chunk.css"
+  },
+  {
+    "revision": "f2370dd14260627921b3",
+    "url": "/karthikkumar19/BurgerApp.git/static/js/2.fa45dc96.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/karthikkumar19/BurgerApp.git/static/js/2.fa45dc96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24cc7a24496ee787fdf5",
+    "url": "/karthikkumar19/BurgerApp.git/static/js/3.197ab16e.chunk.js"
+  },
+  {
+    "revision": "a96e1878552606d3ba8c",
+    "url": "/karthikkumar19/BurgerApp.git/static/js/4.c84e7af4.chunk.js"
+  },
+  {
+    "revision": "69a0f84598621378ee0d",
+    "url": "/karthikkumar19/BurgerApp.git/static/js/5.a3021586.chunk.js"
+  },
+  {
+    "revision": "a7ef5960babbf106638e",
+    "url": "/karthikkumar19/BurgerApp.git/static/js/main.a7d979c4.chunk.js"
+  },
+  {
+    "revision": "fa5f7f71fa612a079557",
+    "url": "/karthikkumar19/BurgerApp.git/static/js/runtime-main.79ad8e8a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/karthikkumar19/BurgerApp.git/static/media/burger-logo.b8503d26.png"
+  }
+]);
